b8502e36d75fd1ebd998e5aaaa85850ef5d51ffc
Irvin <irvinruiz44@yahoo.com>
2022-03-25 18:41:01 -0600

    added js

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+      title: "weather dashboard",
+      subtitle: "JS, HTML, CSS",
+      description:
+        "this is a weather app that allows you to input a city and it will give you the current weather and for the upcoming 5 days",
+      image: "./assets/weather.dashboard.png",
+      link: "https://irvruiz.github.io/Weather-Dashboard/",
+    },
+    {
+      title: "Work Day Scheduler",
+      subtitle: "JS, HTML, CSS",
+      description:
+        "This app allows you to schedule any upcoming events throughout your work day",
+      image: "./assets/work.scheduler.png",
+      link: "https://irvruiz.github.io/Work-Day-Scheduler/",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
